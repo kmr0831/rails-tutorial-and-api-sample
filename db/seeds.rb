a4506cca6c18132ef4986b854cc9a9b1bfa427cd
Ginjiro Kimura <ginjirokimura0831@gmail.com>
2023-08-05 01:21:30 +0000

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+EkycUser.create(uuid: "d89773c9-a45a-991b-af1b-e2ed843b2208", display_name: "鈴木 一郎")
