246b5770bf635f1b3a27a496b20fc74048320ada
Ginjiro Kimura <ginjirokimura0831@gmail.com>
2023-08-15 01:43:47 +0000

    画像投稿前まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_14_010340) do
+ActiveRecord::Schema.define(version: 2023_08_14_051716) do
 
-  create_table "claim_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "verified_claim_id", null: false
-    t.string "street_address"
-    t.string "locality"
-    t.string "region"
-    t.string "postal_code"
-    t.string "country"
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["verified_claim_id"], name: "index_claim_addresses_on_verified_claim_id"
-  end
-
-  create_table "ekyc_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "uuid", limit: 64, null: false
-    t.string "display_name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -47,39 +37,5 @@ ActiveRecord::Schema.define(version: 2023_08_14_010340) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "verification_evidences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "verification_process_id", null: false
-    t.datetime "time"
-    t.string "evidence_type"
-    t.string "check_method"
-    t.string "document_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["verification_process_id"], name: "index_verification_evidences_on_verification_process_id"
-  end
-
-  create_table "verification_processes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "verified_claim_id", null: false
-    t.string "trust_framework"
-    t.datetime "time"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["verified_claim_id"], name: "index_verification_processes_on_verified_claim_id"
-  end
-
-  create_table "verified_claims", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "ekyc_user_id", null: false
-    t.string "name"
-    t.string "given_name"
-    t.string "family_name"
-    t.string "birthdate"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["ekyc_user_id"], name: "index_verified_claims_on_ekyc_user_id"
-  end
-
-  add_foreign_key "claim_addresses", "verified_claims"
-  add_foreign_key "verification_evidences", "verification_processes"
-  add_foreign_key "verification_processes", "verified_claims"
-  add_foreign_key "verified_claims", "ekyc_users"
+  add_foreign_key "microposts", "users"
 end
